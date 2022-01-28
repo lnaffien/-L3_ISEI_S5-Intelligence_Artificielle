1290da6d0e4b3e41011b3fce76d58e9644873f4e
unknown <lucie@naffien.com>
2022-01-28 22:37:07 +0100

    DataExtractor : sexe to int

--- data_extractor.py
+++ data_extractor.py
@@ -11,6 +11,7 @@ from operator import le
 from shutil import ExecError
 import string
 import sys
+from tkinter import E
 
 class DataExtractor:
     DATA_FILE_NAME = "data_baby.csv"
@@ -26,9 +27,10 @@ class DataExtractor:
     def __to_float__(self):
         for i in range(0, len(self.f_array)) : 
             try :
-                (self.f_array[i])["Sexe"] = ord((self.f_array[1])["Sexe"])
-            except Exception :
-                sys.exit("ERROR : DATA EXTRACTION : " + self.DATA_FILE_NAME + " line " + str(i) + " there's str instead of char data in the file.")
+                (self.f_array[i])["Sexe"] = ord((self.f_array[i])["Sexe"])
+            except Exception as e :
+                sys.exit("ERROR : DATA EXTRACTION : " + self.DATA_FILE_NAME + " line " + str(i) + " " + e)
+        print(self.f_array)
 
 
     def get_line(self, line_number):
