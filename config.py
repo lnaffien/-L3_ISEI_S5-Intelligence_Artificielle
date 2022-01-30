1c22cc057a442b65640d1576345c07efb8bf7ea3
unknown <lucie@naffien.com>
2022-01-30 23:16:50 +0100

    Comments

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
+#################################################################################################################
+#                                                                                                               #
+#                                               Config file                                                     #
+#                                                       by NAFFIEN Lucie (January 2022)                         #
+#                                                                                                               #
+# This module is a part of the project made for the "Artificial Intelligence" course, teached by M. DAACHI.     #
+# It is a part of the 3rd Bachelor's Computer Science specialised for Embedded and Interactive Systems year,    #
+# at University Paris 8.                                                                                        #
+#                                                                                                               #
+# GitHub link for the full project : https://github.com/lnaffien/L3_ISEI_S5-Intelligence_Artificielle           #
+#                                                                                                               #
+#################################################################################################################
+
 DATA_FILE_NAME = "data_baby.csv"
 DATA_RESULT_FILE_NAME = "data_result.csv"
 DATA_COLUMNS_CHR_TYPE = ["Sexe"]
