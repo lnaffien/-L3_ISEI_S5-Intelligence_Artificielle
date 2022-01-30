3fc0e5e976986bb2c45c5cb7d273b799c94f41fa
unknown <lucie@naffien.com>
2022-01-30 17:18:55 +0100

    Some cleaning

--- code.py
+++ code.py
@@ -1,24 +1,26 @@
 from random import randrange
-from config import DATA_COLUMN_TO_FIND
+from config import DATA_MAX_LEARNING_ROW
 from data_extractor import DataExtractor
 from data_saved import DataSaved
 from perceptron import Perceptron
 
 data = DataExtractor()
 w = []
+result_data = DataSaved(data.get_input_line(0))
 
 for columnName in data.f_array_input[0]:
         w.append((randrange(0, 10)) / 10)
 
 perceptron = Perceptron(w)
-perceptron.exec_perceptron(data.get_input_line(0))
+for line in range(0, DATA_MAX_LEARNING_ROW - 1) :
+    perceptron.learn(data.get_input_line(line), data.get_output_line(line))
 
-# result = DataSaved(data.get_input_line(1))
-# result.write_line(data.get_input_line(0))
+for line in range(DATA_MAX_LEARNING_ROW, len(data.f_array_input)) :
+    perceptron.predict(data.get_input_line(line))
+    result_data.write_line(data.get_input_line(line))
+
 
 # w = [w0, w1, w2, w3] # semaine, sexe, poids, bias
-
-# data = [0] * 3 # semaine, sexe, poids
 
 # for i in range(497):
 #     # Calcul de x        
--- perceptron.py
+++ perceptron.py
@@ -11,8 +11,8 @@ class Perceptron:
     def __init__(self, w = list):
         self.w = w
     
-    def exec_perceptron(self, data = list):
-        x = self.__x_calcul__(data)
+    def learn(self, data_input = list, data_output = list):
+        x = self.__x_calcul__(data_input)
 
         f = 0
         if PERCEPTRON_ACTIVATION_FUNCTION == SIGMOID :
@@ -22,11 +22,14 @@ class Perceptron:
         else :
             exit("ERROR : CONFIG : PERCEPTRON_ACTIVATION_FUNCTION isn't a valid function.")
 
-        print("f(" + str(x) + ") = " + str(f))
+        #print("f(" + str(x) + ") = " + str(f))
 
         #     print("f(x) = x, so a weight update isn't necessary.")
 
-        self.__weight_update__(data, f)        
+        self.__weight_update__(data_input, data_output, f)
+
+    def predict(self, data_input = list):
+        o = 3
 
         
     def __x_calcul__(self, data = list):
@@ -41,13 +44,12 @@ class Perceptron:
     def __f_calcul_hyperbolic_tangent__(self, x):
         return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
 
-    def __weight_update__(self, data = list, f = float):
-        self.w_bias = self.w_bias + self.alpha * ( - f) * self.bias
-        print(self.w)
+    def __weight_update__(self, data_input = list, data_output = list, f = float):
+        self.w_bias = self.w_bias + self.alpha * (float(list(data_output.values())[0]) - f) * self.bias
         for i in range(0, len(self.w)):
-            print("w" + str(i) + " = " + str(self.w[i]) + " + " + str(self.alpha) + " * (" + str(f) + " - " + str(f) + ") * " + str(list(data.values())[i]) )
-            self.w[i] = self.w[i] + self.alpha * ( - f) * float(list(data.values())[i])
-        print(self.w)
+            #print("w" + str(i) + " = " + str(self.w[i]) + " + " + str(self.alpha) + " * (" + str(list(data_output.values())[0]) + " - " + str(f) + ") * " + str(list(data_input.values())[i]) )
+            self.w[i] = self.w[i] + self.alpha * ( float(list(data_output.values())[0]) - f) * float(list(data_input.values())[i])
+        #print(self.w)
         
 
 
