45bf92da3121ea3112aab102ad7d69416140f6ac
unknown <lucie@naffien.com>
2022-01-30 01:02:39 +0100

    DataExtractor adapted to config file

--- data_extractor.py
+++ data_extractor.py
@@ -6,7 +6,7 @@
 
 import csv
 import sys
-from config import DATA_FILE_NAME
+from config import DATA_FILE_NAME, DATA_COLUMNS_CHR_TYPE
 
 class DataExtractor:
     f_array = None
@@ -19,11 +19,12 @@ class DataExtractor:
 
 
     def __to_float__(self):
-        for i in range(0, len(self.f_array)) : 
-            try :
-                (self.f_array[i])["Sexe"] = ord((self.f_array[i])["Sexe"])
-            except Exception as e :
-                sys.exit("ERROR : DATA EXTRACTION : " + DATA_FILE_NAME + " line " + str(i) + " " + e)
+        for line in range(0, len(self.f_array)) :
+            for column in DATA_COLUMNS_CHR_TYPE :            
+                try :
+                    (self.f_array[line])[column] = ord((self.f_array[line])[column])
+                except Exception as e :
+                    sys.exit("ERROR : DATA EXTRACTION : " + DATA_FILE_NAME + " line " + str(line) + " column " + str(column) + e)
 
 
     def get_line(self, line_number):
